8f17a8425f8473bde234819d72c11eac5962d1af
MateuszMoskal <mateusz.moskal07@wp.pl>
2025-05-20 14:36:12 +0200

    Testy dzialan

---
+++ Pliki/utils.py
@@ -0,0 +1,30 @@
+# przykladowe funkcje utils .py
+
+def add ( a: int , b: int) -> int:
+    '''
+    :param a: first number
+    :param b: second number
+    :return: sum of two numbers
+    '''
+    return a + b
+def subtract ( a: int , b: int) -> int:
+    '''
+    :param a: first number
+    :param b: second number
+    :return: substract of two numbers
+    '''
+    return a - b
+def multiply ( a: int , b: int) -> int:
+    '''
+    :param a: first number
+    :param b: second number
+    :return: multiply of two numbers
+    '''
+    return a * b
+def divide ( a: int , b: int) -> float :
+    '''
+    :param a: first number
+    :param b: second number
+    :return: divide of two numbers
+    '''
+    return a / b
